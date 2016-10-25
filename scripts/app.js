0a7690495f11d3aaf9209adc7b1f7458ae793e7a
Le Thuy Hang <thuyhangle.le@gmail.com>
2016-10-25 03:20:12 +0300

    Test output

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+var test1 = new Vue({
+    el: '#test1',
+    data: {
+        text1: 'Hello VeuJS!'
+    }
+})
+
+var test2 = new Vue({
+    el: "#test2",
+    data: {
+        text2: "You loaded this page on " + new Date()
+    }
+})
+
+var test3 = new Vue({
+    el: "#test3",
+    data: {
+        seen: true
+    }
+})
